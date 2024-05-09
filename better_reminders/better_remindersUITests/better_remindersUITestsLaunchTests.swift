c2b4f8d47376896b2b8898d8d2db6e9f8b595e94
Liam Tan <liamisaactan@gmail.com>
2024-05-09 19:27:33 +1000

    take 2. add new swift xcode project.

---
+++ better_reminders/better_remindersUITests/better_remindersUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  better_remindersUITestsLaunchTests.swift
+//  better_remindersUITests
+//
+//  Created by Liam Tan on 9/5/2024.
+//
+
+import XCTest
+
+final class better_remindersUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
